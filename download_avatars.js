413d77c3b494005083c5b6b00fff85e76617f591
Sadie <sadiefreeman@gmail.com>
2018-03-20 00:50:44 +0000

    Added authentication token to point to secrets file

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,31 @@
 var request = require('request');
+var secrets = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Auth': 'token:' + secrets.GITHUB_TOKEN
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+
+// function noData() {
+//   console.log("No avatars available")
+// }
+
+
+
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
