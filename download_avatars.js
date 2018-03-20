23724ac240b8dc47c200da674bd6da7e54374592
Sadie <sadiefreeman@gmail.com>
2018-03-20 02:29:13 +0000

    Parsed JSON format, added for each loop in request function

--- download_avatars.js
+++ download_avatars.js
@@ -1,31 +1,33 @@
 var request = require('request');
 var secrets = require('./secrets.js');
 
+
 console.log('Welcome to the GitHub Avatar Downloader!');
-
 function getRepoContributors(repoOwner, repoName, cb) {
-
   var options = {
+    json: true,
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Auth': 'token:' + secrets.GITHUB_TOKEN
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN
     }
   };
+
   request(options, function(err, res, body) {
     cb(err, body);
+    body.forEach(function (object) {
+      console.log(object.avatar_url);
+    });
   });
-
-// function noData() {
-//   console.log("No avatars available")
-// }
-
-
-
-
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+
+
+
+
+
